2f360302afddf67e642f0d50bf4fa58438c3a375
fraliva <fraliva@gmail.com>
2020-02-20 23:04:15 -0300

    Criado e removido atributos dinâmicos de objeto do tipo pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -11,10 +11,24 @@ if __name__=='__main__':
     guilherme = Pessoa(nome='Guilherme')
     gabriel = Pessoa(nome='Gabriel')
     francisco = Pessoa(guilherme, gabriel, nome='Francisco')
+
     print(Pessoa.cumprimentar(francisco))
     print(id(francisco))
     print(francisco.cumprimentar())
     print(francisco.nome)
     print(francisco.idade)
+
     for filho in francisco.filhos:
         print(filho.nome)
+
+    # Atributo criado dinamicamente - não é uma boa prática
+    francisco.sobrenome = 'Valente'
+    print(francisco.sobrenome)
+
+    # É possível remover os atributos dinamicamente também
+    del francisco.filhos
+
+    print(francisco.__dict__)
+    print(guilherme.__dict__)
+    print(gabriel.__dict__)
+
